716d688531228b9884712102c6b3c2bacc62e97a
Dhaneyl <dha01rr@gmail.com>
2022-08-01 00:23:33 -0400

    Rendering images solution

--- src/Heroes/components/HeroList.jsx
+++ src/Heroes/components/HeroList.jsx
@@ -11,12 +11,12 @@ export const HeroList = ({publisher}) => {
     <div className='row rows-cols-1 row-cols-md-3 g-3'>
 
     { 
-      heroes.map(hero=>{
-       return  <HeroCard 
+      heroes.map(hero=>(
+       <HeroCard 
           key={hero.id}
           {...hero}
         />
-    })
+      ))
     
     }
 
